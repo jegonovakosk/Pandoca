2b3e89a60385f87d974a72ed76a919e976d475d4
jegonovakosk <62294366+jegonovakosk@users.noreply.github.com>
2023-07-03 15:04:14 -0300

    sistema da pandoca

---
+++ src/Page/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import BannerHome from '../../components/Banner'
+
+const Home = () => {
+  return (
+    <div>
+        <BannerHome/>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/components/Banner/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Banner from '../../Image/banner/banner.webp'
+
+const BannerHome = () => {
+  return (
+    <div children='container'>
+      <img src={Banner} alt="Banner joias Pandoca"  width="100%"/>
+      <section>
+        vitrine
+      </section>
+    </div>
+  )
+}
+
+export default BannerHome
